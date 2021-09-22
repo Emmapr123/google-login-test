8cb25533444b03f41fa7c1b9a75ade3a5c8afdaa
Emmapr123 <emmapriester@hotmail.com>
2021-09-22 10:09:32 +0100

    Google login button created

--- src/components/login-button/login-button.js
+++ src/components/login-button/login-button.js
@@ -1,1 +1,29 @@
 import React from 'react';
+import { GoogleLogin } from 'react-google-login';
+
+const clientId = '352290173564-okpkhguvofsdpo5nsc6esu5u3epgkioh.apps.googleusercontent.com';
+
+const LoginButton = () => {
+    const onSucces = (result) => {
+        console.log('[Login succes] current user: ', result.profileObj)
+    }
+
+    const onFailure = (result) => {
+        console.log('[Login failed result', result)
+    }
+
+    return(
+        <div>
+            <GoogleLogin
+                clientId={clientId}
+                buttonText={'Login'}
+                onSucces={onSucces}
+                onFailure={onFailure}
+                cookiePolicy={'single_host_origin'}
+                isSignedIn={true}
+            />
+        </div>
+    )
+}
+
+export { LoginButton }
